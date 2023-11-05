8ac25b168846bcfdc9ae8467412ecf49dcb5a474
TefDev <tefgoov@gmail.com>
2023-11-05 12:50:06 -0600

    Working Lever
    Lever rotates and plays sound as intended, still missing timer and events

--- Assets/_Scripts/GameScripts/LeverRotation.cs
+++ Assets/_Scripts/GameScripts/LeverRotation.cs
@@ -1,18 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LeverRotation : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float rotationSpeed = 100.0f;
+    public AudioClip crankSound;
+
+    private bool isGrabbed = false;
+    private AudioSource audioSource;
+
     void Start()
     {
-        
+        audioSource = GetComponent <AudioSource>();
+        audioSource.clip = crankSound;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isGrabbed)
+        {
+            // Rotate the crank in the negative Z-axis.
+            transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            if (audioSource.isPlaying)
+            {
+                audioSource.Pause();
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!isGrabbed)
+            {
+                isGrabbed = true;
+                if (!audioSource.isPlaying)
+                {
+                    audioSource.UnPause();
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isGrabbed = false;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        isGrabbed = true;
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
     }
 }
